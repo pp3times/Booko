f56c842f2833746d7f58c955baa492539eb9b5ab
Jeremy <than2314@gmail.com>
2022-04-07 19:51:22 +0700

    refactor: 💡 re create with router

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-# bookplay
+# bookko
 
 ## Project setup
 ```
-pnpm install
+yarn install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-pnpm run serve
+yarn serve
 ```
 
 ### Compiles and minifies for production
 ```
-pnpm run build
+yarn build
 ```
 
 ### Lints and fixes files
 ```
-pnpm run lint
+yarn lint
 ```
 
 ### Customize configuration
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="to Your Wongsapat App" />
-  <HelloWorld msg="Hi Puriwat" />
-  <Wongsapat />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import Wongsapat from "./components/Wongsapat.vue";
-import Puriwat from './components/Puriwat.vue'
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    Wongsapat,
-    Puriwat
-  },
-};
-</script>
 
 <style>
 #app {
@@ -27,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
