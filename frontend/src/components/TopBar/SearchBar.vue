48bc0862abba461335b27464e203ef9ad3c2d6dd
Jeremy <than2314@gmail.com>
2022-04-10 22:54:26 +0700

    fix: 🐛 fix category

--- frontend/src/components/TopBar/SearchBar.vue
+++ frontend/src/components/TopBar/SearchBar.vue
@@ -29,7 +29,6 @@
           height="24"
           viewBox="0 0 24 24"
           class="fill-white"
-					v-if="isHidden"
         >
           <path
             d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z"
