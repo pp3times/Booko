6b41c99183eb3ebe6a8642f000d5fdf8b693179f
Jeremy <than2314@gmail.com>
2022-04-11 00:38:19 +0700

    style: 💄 create template login

--- frontend/src/components/TopBar/SearchBar.vue
+++ frontend/src/components/TopBar/SearchBar.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="">
-    <!-- <label class="relative block">
-      <span class="sr-only">Search</span>
-      <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-        <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20">
-          x
-        </svg>
-      </span>
-      <input
-        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-        placeholder="Search for anything..."
-        type="text"
-        name="search"
-      />
-    </label> -->
-
-    <!-- page -->
-
     <main class="flex w-full items-center justify-center bg-black pb-2 px-24">
       <!-- component -->
       <div
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#01b266",
+        secondary: "#01cd76",
+      },
+    },
   },
   plugins: [],
 };
